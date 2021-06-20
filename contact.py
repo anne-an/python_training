a40c77204c34f567c06ee485a86df394b431e6d6
anne_morke <anne.morke@gmail.com>
2021-06-20 17:06:53 +0300

    Add create contact test

---
+++ contact.py
@@ -0,0 +1,19 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone,
+                       mobile_phone, work_phone, fax, email, bday, bmonth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
