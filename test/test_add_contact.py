8aa59ce51eb28361a309d48137f2b0bdb209ffbc
anne_morke <anne.morke@gmail.com>
2021-06-24 23:13:59 +0300

    Refactor fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(u"Олег", u"Олегович", u"Котиков", u"Котик", "HR", "Auriga",
                                u"Санкт-Петербург, Невский проспект, дом 100", "123456", "78998887766", "654321",
                                "123321",
                                "oleg.kotikov@gmail.com", "6", "September", "1990", "", "", ""))
-    app.session.logout()
