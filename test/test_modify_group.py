a6d06d500c14639a074cd66e27921d4db6d1a8b4
anne_morke <anne.morke@gmail.com>
2021-06-27 19:02:17 +0300

    Add assertions to group tests

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,15 +2,28 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_if_no_groups()
-    app.group.modify_first_group(Group(name="qwerty", header="asdzxc123", footer=""))
+    group = Group(name="qwerty", header="asdzxc123", footer="")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_name(app):
-    app.group.create_if_no_groups()
-    app.group.modify_first_group(Group(name="New group"))
-
-
-def test_modify_group_header(app):
-    app.group.create_if_no_groups()
-    app.group.modify_first_group(Group(header="New header"))
+# def test_modify_group_name(app):
+#     old_groups = app.group.get_group_list()
+#     app.group.create_if_no_groups()
+#     app.group.modify_first_group(Group(name="New group"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+#
+# def test_modify_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     app.group.create_if_no_groups()
+#     app.group.modify_first_group(Group(header="New header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
