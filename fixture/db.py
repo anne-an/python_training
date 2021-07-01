3a7d143970f196055142eabab35eb6bbad885207
anne_morke <anne.morke@gmail.com>
2021-07-01 19:11:27 +0300

    Add test for all contact data

--- fixture/db.py
+++ fixture/db.py
@@ -1,3 +1,5 @@
+import re
+
 import pymysql
 from model.contact import Contact
 from model.group import Group
@@ -35,6 +37,40 @@ class DbFixture:
         finally:
             cursor.close()
         return list
+
+    def get_all_contact_ids(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id from addressbook where deprecated='0000-00-00 00:00:00'")
+            for id in cursor.fetchall():
+                list.append(id[0])
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_by_id(self, id):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, email, email2, email3, home, mobile, work, "
+                           "phone2 from addressbook where id=%s" % id)
+            contact_info = cursor.fetchall()[0]
+            (id, firstname, lastname, address, email, email2, email3, home, mobile, work, phone2) = contact_info
+            contact = Contact(id=id, firstname=firstname, lastname=lastname, address=address,
+                              all_emails_from_home_page="\n".join(filter(lambda x: x != "", [email, email2, email3])),
+                              all_phones_from_home_page=self.merge_phones(home, mobile, work, phone2))
+        finally:
+            cursor.close()
+        return contact
+
+    def merge_phones(self, home, mobile, work, phone2):
+        def clear(s):
+            return re.sub("[() -]", "", s)
+
+        return "\n".join(filter(lambda x: x != "",
+            map(lambda x: clear(x),
+                filter(lambda x: x is not None,
+                       [home, mobile, work, phone2]))))
 
     def destroy(self):
         self.connection.close()
--- test/test_contact.py
+++ test/test_contact.py
@@ -10,3 +10,15 @@ def test_contact_on_edit_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == contact_from_edit_page.all_emails_from_home_page
     assert contact_from_home_page.all_phones_from_home_page == contact_from_edit_page.all_phones_from_home_page
+
+
+def test_contact_in_db(app, db):
+    ids = db.get_all_contact_ids()
+    for id in ids:
+        contact_from_home_page = app.contact.get_contact_by_id(id)
+        contact_from_db = db.get_contact_by_id(id)
+        assert contact_from_home_page.firstname == contact_from_db.firstname
+        assert contact_from_home_page.lastname == contact_from_db.lastname
+        assert contact_from_home_page.address == contact_from_db.address
+        assert contact_from_home_page.all_emails_from_home_page == contact_from_db.all_emails_from_home_page
+        assert contact_from_home_page.all_phones_from_home_page == contact_from_db.all_phones_from_home_page
