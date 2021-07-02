463f89e3ea7f1e118b2df837514a41aafd77ac31
anne_morke <anne.morke@gmail.com>
2021-07-02 15:44:02 +0300

    Fix contacts condition

--- check_db_connection.py
+++ check_db_connection.py
@@ -4,7 +4,7 @@ from model.group import Group
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_not_empty_groups()
+    l = db.get_contact_list()
     for item in l:
         print(item)
     print(len(l))
