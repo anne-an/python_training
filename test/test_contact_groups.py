2d19240522da5a884fedbb1c02781b6c13a54268
anne_morke <anne.morke@gmail.com>
2021-07-04 13:10:54 +0300

    Check contacts in group len after adding contact to group

--- test/test_contact_groups.py
+++ test/test_contact_groups.py
@@ -1,17 +1,24 @@
 import random
+
+from model.contact import Contact
 from model.group import Group
 
 
 def test_add_contact_to_group(app, orm):
     app.group.create_if_no_groups(orm)
     app.contact.create_if_no_contacts(orm)
-    contacts = orm.get_contact_list()
-    contact = random.choice(contacts)
     groups = orm.get_group_list()
     group = random.choice(groups)
+    contacts = orm.get_contacts_not_in_group(Group(id=group.id))
+    if not contacts:
+        app.contact.create(Contact(firstname="Name"))
+        contacts = orm.get_contacts_not_in_group(Group(id=group.id))
+    contact = random.choice(contacts)
+    old_contacts_in_group = list(orm.get_contacts_in_group(Group(id=group.id)))
     app.contact.add_to_group_by_id(contact.id, group.name)
-    contacts_in_group = list(orm.get_contacts_in_group(Group(id=group.id)))
-    assert contact in contacts_in_group
+    new_contacts_in_group = list(orm.get_contacts_in_group(Group(id=group.id)))
+    assert contact in new_contacts_in_group
+    assert len(old_contacts_in_group) + 1 == len(new_contacts_in_group)
 
 
 def test_delete_contact_from_group(app, orm):
